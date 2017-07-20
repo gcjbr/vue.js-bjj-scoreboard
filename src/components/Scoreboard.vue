8cf0c96accaaf1d2beba690edeafea508baececf
Gustavo Caetano <souljacker@gmail.com>
2017-07-20 01:01:35 -0300

    refactored css and html structure to fit better on small screens

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -2,15 +2,26 @@
  <div class="scoreboard">
       <div class="fighter fighter1">
         <div class="fighter-points">{{ fighter1.score }}</div>
-        <div class="adv">{{ fighter1.advantage }}</div>
-        <div class="fau">{{ fighter1.fault }}</div>
+
       </div>
     <div class="fighter fighter2">
       <div class="fighter-points">{{ fighter2.score }}</div>
-      <div class="adv">{{ fighter2.advantage }}</div>
-      <div class="fau">{{ fighter2.fault }}</div>
+
     </div>
-      <div class="chronometer"> <span>{{ time }} </span></div>
+    <div class="score-sub">
+      <div class="sub-fighter sub-fighter1">
+        <div class="adv">{{ fighter1.advantage }}</div>
+        <div class="fau">{{ fighter1.fault }}</div>
+      </div>
+      <div class="chronometer">
+        <span>{{ time }} </span>
+      </div>
+      <div class="sub-fighter  sub-fighter2">
+        <div class="adv">{{ fighter2.advantage }}</div>
+        <div class="fau">{{ fighter2.fault }}</div>
+      </div>
+    </div>
+
 </div>
 
 </template>
