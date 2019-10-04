cc1512f9ce1a602f069ad6bc46b9abddb64dc331
Gustavo Caetano <souljacker@gmail.com>
2019-10-04 15:29:04 -0300

    Change fault to penalty

--- src/components/ControlPoints.vue
+++ src/components/ControlPoints.vue
@@ -5,14 +5,14 @@
           <div class="btn" @click="changeTo(target,'score', 3)">+ 3</div>
           <div class="btn" @click="changeTo(target,'score', 2)">+ 2</div>
           <div class="btn" @click="changeTo(target,'advantage', 1)">+ adv</div>
-          <div class="btn" @click="changeTo(target,'fault', 1)">+ fault</div>
+          <div class="btn" @click="changeTo(target,'fault', 1)">+ penal</div>
         </div>
         <div class="minus">
           <div class="btn" @click="changeTo(target,'score', -4)">- 4</div>
           <div class="btn" @click="changeTo(target,'score', -3)">- 3</div>
           <div class="btn" @click="changeTo(target,'score', -2)">- 2</div>
           <div class="btn" @click="changeTo(target,'advantage', -1)">- adv</div>
-          <div class="btn" @click="changeTo(target,'fault', -1)">- fault</div>
+          <div class="btn" @click="changeTo(target,'fault', -1)">- penal</div>
         </div>
       </div>
 
